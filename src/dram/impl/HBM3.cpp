bfd9b46d66db4ea9c0112b1c687e9467e1953318
Yunseon Shin <yunseon0518@naver.com>
2025-06-10 13:49:35 +0900

    [HBM3] add timing param for 2.4Gbps

--- src/dram/impl/HBM3.cpp
+++ src/dram/impl/HBM3.cpp
@@ -16,7 +16,8 @@ class HBM3 : public IDRAM, public Implementation {
 
     inline static const std::map<std::string, std::vector<int>> timing_presets = {
       //   name       rate   nBL  nCL  nRCDRD  nRCDWR  nRP  nRAS  nRC  nWR  nRTPS  nRTPL  nCWL  nCCDS  nCCDL  nRRDS  nRRDL  nWTRS  nWTRL  nRTW  nFAW  nRFC  nRFCSB  nREFI  nREFISB  nRREFD  tCK_ps
-      {"HBM3_2Gbps",  {2000,   4,   7,    7,      7,     7,   17,  19,   8,    2,     3,    2,    1,      2,     2,     3,     3,     4,    3,    15,   -1,   160,   3900,     -1,      8,   1000}},
+      {"HBM3_2Gbps",  {2000,   2,   7,    7,      7,     7,   17,  19,   8,    2,     3,    2,    1,      2,     2,     3,     3,     4,    3,    15,   -1,   160,   3900,     -1,      8,   1000}},
+      {"HBM3_2.4Gbps",  {2400,   2,   9,    9,      9,     9,   21,  23,  10,    3,     4,    3,    1,      2,     3,     4,     4,     5,    4,    19,   -1,   193,   4680,     -1,     10,    833}},
       // TODO: Find more sources on HBM3 timings...
     };
 
@@ -24,7 +25,7 @@ class HBM3 : public IDRAM, public Implementation {
   /************************************************
    *                Organization
    ***********************************************/   
-    const int m_internal_prefetch_size = 2;
+    const int m_internal_prefetch_size = 4;
 
     inline static constexpr ImplDef m_levels = {
       "channel", "pseudochannel", "bankgroup", "bank", "row", "column",    
